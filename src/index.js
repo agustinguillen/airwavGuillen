b9c644f19fd108dbf683d12ecf57e316d59e594c
Agustin Guillen <agustinnguillen@gmail.com>
2021-08-01 21:06:39 -0300

    Entrega final del proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "jquery";
-import "popper.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 import App from "./App";
